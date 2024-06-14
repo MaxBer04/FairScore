1e31398b9eb63acedf96115db9426fbdfcde1525
Max <kbertram6@googlemail.com>
2024-06-14 14:28:00 +0000

    more changes

--- SD1_5/s3_util.py
+++ SD1_5/s3_util.py
@@ -3,6 +3,7 @@ import argparse
 import shutil
 import boto3
 import csv
+from tqdm import tqdm
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,19 +14,24 @@ def compress_dataset(dataset_dir, compressed_file):
 
 def upload_to_s3(compressed_file, bucket_name, access_key_id, secret_access_key):
     s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+    file_size = os.path.getsize(compressed_file + '.zip')
     print(f"Uploading '{compressed_file}' to S3 bucket '{bucket_name}'...")
-    s3.upload_file(compressed_file + '.zip', bucket_name, os.path.basename(compressed_file) + '.zip')
+    with tqdm(total=file_size, unit='B', unit_scale=True, desc='Uploading') as pbar:
+        s3.upload_file(compressed_file + '.zip', bucket_name, os.path.basename(compressed_file) + '.zip', Callback=lambda bytes_transferred: pbar.update(bytes_transferred))
     print("Dataset uploaded to S3 successfully.")
 
 def download_from_s3(bucket_name, compressed_file, access_key_id, secret_access_key):
     s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
     print(f"Downloading '{compressed_file}' from S3 bucket '{bucket_name}'...")
-    s3.download_file(bucket_name, compressed_file + '.zip', compressed_file + '.zip')
+    with tqdm(unit='B', unit_scale=True, desc='Downloading') as pbar:
+        s3.download_file(bucket_name, compressed_file + '.zip', compressed_file + '.zip', Callback=lambda bytes_transferred: pbar.update(bytes_transferred))
     print("Dataset downloaded from S3 successfully.")
 
 def extract_dataset(compressed_file, dataset_dir):
     print(f"Extracting '{compressed_file}' to '{dataset_dir}'...")
-    shutil.unpack_archive(compressed_file + '.zip', dataset_dir)
+    with tqdm(unit='file', desc='Extracting') as pbar:
+        shutil.unpack_archive(compressed_file + '.zip', dataset_dir)
+        pbar.update(1)
     print("Dataset extracted successfully.")
 
 def main():
