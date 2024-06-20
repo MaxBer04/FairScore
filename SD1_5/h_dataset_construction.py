e2204a1a8802a5302c49ab3cd5361d65da88fe83
Max <kbertram6@googlemail.com>
2024-06-20 09:47:18 +0000

    Some changes

--- SD1_5/h_dataset_construction.py
+++ SD1_5/h_dataset_construction.py
@@ -37,26 +37,29 @@ def generate_prompts(occupation, batch_size):
 
 def save_dataset(dataset, output_dir):
     logger.log(f"\nSaving dataset to {output_dir}...")
-    for key, data in dataset.items():
-        print(type(data))
-        print(data[0])
-        # Speichere h_vects als NumPy-Arrays
-        np.savez(os.path.join(output_dir, f"h_vects.npz"), **{str(t): v for t, v in data["h_vects"].items()})
-        
-        # Speichere Metadaten in einer JSON-Datei
-        with open(os.path.join(output_dir, "metadata.json"), "w") as f:
-            json.dump({
-                "normal_prompts": data["normal_prompts"],
-                "sensitive_prompts": data["sensitive_prompts"],
-                "face_detected": data["face_detected"],
-                "gender_probs": data["gender_probs"],
-                "occupations": data["occupations"]
-            }, f)
+    
+    # Speichere h_vects als NumPy-Arrays
+    h_vects_dict = {t: [] for t in range(51)}
+    for img_h_vects in dataset["h_vects"]:
+        for t, h_vect in enumerate(img_h_vects):
+            h_vects_dict[t].append(h_vect.cpu().numpy())
+    for t, h_vects in h_vects_dict.items():
+        np.savez(os.path.join(output_dir, f"h_vects_t{t}.npz"), h_vects=h_vects)
+
+    # Speichere Metadaten in einer JSON-Datei
+    with open(os.path.join(output_dir, "metadata.json"), "w") as f:
+        json.dump({
+            "normal_prompts": dataset["normal_prompts"],
+            "sensitive_prompts": dataset["sensitive_prompts"],
+            "face_detected": dataset["face_detected"],
+            "gender_probs": dataset["gender_probs"],
+            "occupations": dataset["occupations"]
+        }, f)
 
 def plot_images(images, faces, boxes, gender_probs, batch_idx, output_dir, max_images):
     num_images = len(images)
     if max_images == 0:
-      return
+        return
     if num_images > max_images:
         idxs = random.sample(range(num_images), max_images)
         images = [images[i] for i in idxs]
@@ -70,7 +73,7 @@ def plot_images(images, faces, boxes, gender_probs, batch_idx, output_dir, max_i
 
     for i, (img, face, box, gender_prob) in enumerate(zip(images[:max_images], faces[:max_images], boxes[:max_images], gender_probs[:max_images])):
         if not isinstance(img, th.Tensor):
-          img = ToTensor()(img)
+            img = ToTensor()(img)
         img = img.cpu().numpy().transpose(1, 2, 0)
         img = (img * 255).astype(np.uint8)
         img = Image.fromarray(img)
@@ -163,14 +166,20 @@ def main():
                     else:
                         gender_probs_batch.append([])
 
-                for h_vect, normal_prompt, sensitive_prompt, face_detected, gender_prob in zip(h_vects, normal_prompts, sensitive_prompts, [face is not None for face in faces], gender_probs_batch):
-                    dataset["h_vects"].append(h_vect.cpu())
-                    dataset["sensitive_prompts"].append(sensitive_prompt)
-                    dataset["normal_prompts"].append(normal_prompt)
-                    dataset["face_detected"].append(face_detected)
-                    dataset["gender_probs"].append(gender_prob)
-                    dataset["occupations"].append(occupation)
+                # Speichere h_vects als Liste von Listen, wobei jede Unter-Liste die h_vects für ein Bild enthält
+                h_vects_list = [[] for _ in range(batch_size)]
+                for t in h_vects.keys():
+                    for i in range(batch_size):
+                        h_vects_list[i].append(h_vects[t][i*2:i*2+2])
+                dataset["h_vects"].extend(h_vects_list)
+
+                dataset["sensitive_prompts"].extend(sensitive_prompts)
+                dataset["normal_prompts"].extend(normal_prompts)
+                dataset["face_detected"].extend([face is not None for face in faces])
+                dataset["gender_probs"].extend(gender_probs_batch)
+                dataset["occupations"].extend([occupation] * batch_size)
                 
+                print(batch_size, accelerator.num_processes)
                 progress_bar.update(batch_size*accelerator.num_processes)
 
                 # Plotte Bilder nach jedem x-ten Batch
@@ -182,7 +191,6 @@ def main():
     accelerator.wait_for_everyone()
     gathered_dataset = gather_object([dataset])
     if accelerator.is_main_process:
-        custom_dict = {}
         custom_dict = {"h_vects": [], "sensitive_prompts": [], "normal_prompts": [], "face_detected": [], "gender_probs": [], "occupations": []}
         for dataset in gathered_dataset:
             custom_dict["h_vects"].extend(dataset["h_vects"])
@@ -200,14 +208,14 @@ def main():
 
 def create_argparser():
     defaults = dict(
-        num_samples=12,  # Pro Occupation
-        batch_size=12,
+        num_samples=100,  # Pro Occupation
+        batch_size=64,
         use_fp16=True,
         seed="",
         output_dir="dataset",
         model_id="SG161222/Realistic_Vision_V2.0",
-        plot_every=0,  # per batch
-        num_example_images=0 # should be square
+        plot_every=5,  # per batch
+        num_example_images=36 # should be square
     )
     parser = argparse.ArgumentParser()
     for k, v in defaults.items():
